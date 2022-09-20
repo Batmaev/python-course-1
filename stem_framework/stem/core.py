beba392830dbfe57c293900f1b9ae2c3445911b2
Batmaev <vbbatmaev@gmail.com>
2022-09-20 21:18:15 +0300

    Протокол Dataclass для объектов, созданных декоратором @dataclass

--- stem_framework/stem/core.py
+++ stem_framework/stem/core.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Any, Protocol
 
 
 def pascal_case_to_snake_case(name: str) -> str:
@@ -14,5 +14,11 @@ class Named:
         ... # TODO()
 
 
-class Dataclass:
-    ...  # TODO()
+class Dataclass(Protocol):
+    __dataclass_fields__: Any
+    #
+    # В Cython есть функция dataclasses.is_dataclass, 
+    # которая проверяет наличие поля __dataclass_fields__
+    # https://github.com/python/cpython/blob/3.10/Lib/dataclasses.py
+    #
+    # Мой протокол тоже проверяет наличие этого поля.
