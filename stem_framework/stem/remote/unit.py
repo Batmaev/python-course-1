7f8bbe59389d6ff7cdf058d89905b4c680a4ccdf
Batmaev <vbbatmaev@gmail.com>
2022-12-08 13:30:42 +0300

    task 10

--- stem_framework/stem/remote/unit.py
+++ stem_framework/stem/remote/unit.py
@@ -1,30 +1,108 @@
+import json
 import logging
-import os
-import socketserver
-from socketserver import StreamRequestHandler
-from typing import Optional
+from socketserver import StreamRequestHandler, TCPServer
+from threading import Thread
+from multiprocessing import Process
+from typing import Optional, Tuple, Type
 
 from stem.envelope import Envelope
-from stem.task_master import TaskMaster
-from stem.task_runner import SimpleRunner
+from stem.task_master import TaskMaster, TaskStatus
 from stem.task_tree import TaskTree
 from stem.workspace import IWorkspace
-from multiprocessing import Process
+from stem.meta import get_meta_attr
 
 
 class UnitHandler(StreamRequestHandler):
-    workspace: IWorkspace
-    task_tree: TaskTree = TaskTree()
-    task_master = TaskMaster(SimpleRunner(), task_tree)
-    powerfullity: int
+    workspace: IWorkspace | Type[IWorkspace]
+    task_tree: TaskTree | None
+    task_master: TaskMaster
+    powerfullity: int | None
 
     def handle(self) -> None:
-        pass  # TODO(Assigment 10)
+        logging.debug('handle entered')
+        self.envelope = Envelope.read(self.rfile)
+
+        logging.debug(f"server receives command: {get_meta_attr(self.envelope.meta, 'command')}")
+
+        match get_meta_attr(self.envelope.meta, 'command'):
+            case 'run':
+                task_path = get_meta_attr(self.envelope.meta, 'task_path')
+                task = self.workspace.find_task(task_path)
+                if task is None:
+                    resp = Envelope({
+                        'status': 'failed',
+                        'error' : 'Task not found'
+                    })
+                else:
+                    res = self.task_master.execute(
+                        get_meta_attr(self.envelope.meta, 'task_meta', {}),
+                        task,
+                        self.workspace
+                    )
+                    if res.status == TaskStatus.CONTAINS_DATA:
+                        data = res.lazy_data()
+                        resp = Envelope(
+                            {'status': 'fulfilled'},
+                            json.dumps(data).encode('utf8')
+                        )
+                    else:
+                        resp = Envelope({
+                            'status': 'failed',
+                            'error' : res.status
+                        })
+
+            case 'structure':
+                structure = self.workspace.structure()
+                resp = Envelope(
+                    {'status': 'fulfilled'},
+                    json.dumps(structure).encode('utf8')
+                )
+
+            case 'powerfullity':
+                resp = Envelope({
+                    'status': 'fulfilled',
+                    'powerfullity': self.powerfullity
+                })
+
+            # case 'stop':
+            #     resp = Envelope({'status': 'recieved'})               # не работает
+            #     self.wfile.write(resp.to_bytes())
+            #     # We should call server.shutdown from a new thread,
+            #     # otherwise it will result in deadlock
+            #     thread = Thread(target = self.server.shutdown)
+            #     thread.start()
+            #     thread.join()
+            #     return
+
+            case _:
+                resp = Envelope({
+                    'status': 'failed',
+                    'error' : 'input_envelope.meta.command = ???'
+                })
+
+        logging.info(f'server sends: {resp.meta}')
+
+        self.wfile.write(resp.to_bytes()) # если вместо этого написать resp.write_to(self.wfile),
+        self.wfile.flush()                # то будет несколько вызовов wfile.write,
+                                          # но клиент отключается после первого
 
 
-def start_unit(workspace: IWorkspace, host: str, port: int, powerfullity: Optional[int] = None):
-    pass  # TODO(Assigment 10)
+def start_unit(workspace: IWorkspace, host: str, port: int, powerfullity: int | None = None) -> TCPServer:
+    UnitHandler.workspace = workspace
+    UnitHandler.task_tree = None
+    UnitHandler.task_master = TaskMaster()
+    UnitHandler.powerfullity = powerfullity
+    TCPServer.allow_reuse_address = True # иначе нужно будет ждать минуту, прежде чем перезапускать сервер
+    return TCPServer((host, port), UnitHandler)
 
 
-def start_unit_in_subprocess(workspace: IWorkspace, host: str, port: int, powerfullity: Optional[int] = None) -> Process:
-    pass  # TODO(Assigment 10)
+def start_unit_in_subprocess(workspace: IWorkspace, host: str, port: int, powerfullity: Optional[int] = None) -> Tuple[Thread, TCPServer]:
+    server = start_unit(workspace, host, port, powerfullity)
+    thread = Thread(target = server.serve_forever)
+    thread.start()
+
+    logging.debug('thread started')
+
+    return thread, server
+
+# в process не получилось, т.к. pickle
--- stem_framework/tests/remote/test_unit.py
+++ stem_framework/tests/remote/test_unit.py
@@ -1,44 +1,51 @@
-import io
-import logging
 import socket
-import time
+from time import sleep
 from unittest import TestCase
 
 from stem.envelope import Envelope
+from stem.meta import get_meta_attr
 from stem.remote.unit import start_unit_in_subprocess, start_unit
 from tests.example_workspace import IntWorkspace
 
 POWERFULLITY = 5
 HOST = "localhost"
 PORT = 9801
+
+import logging
+logging.basicConfig(format='%(message)s')
 logging.root.setLevel(logging.DEBUG)
 
 
 class ServerUnitTest(TestCase):
     def test_start_unit(self):
-        start_unit(IntWorkspace, HOST, PORT, POWERFULLITY)
+        server = start_unit(IntWorkspace, HOST, PORT, POWERFULLITY)
+        server.server_close()
 
 
 
 class UnitHandlerTest(TestCase):
 
     def setUp(self) -> None:
-        self.process = start_unit_in_subprocess(IntWorkspace, HOST, PORT, POWERFULLITY)
-        time.sleep(3.0) # Wait start server
+        self.process, self.server = start_unit_in_subprocess(IntWorkspace, HOST, PORT, POWERFULLITY)
+        sleep(3.0) # Wait start server
+        logging.debug('setup completed')
 
     def _send(self, envelope: Envelope):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             sock.connect((HOST, PORT))
             sock.sendall(envelope.to_bytes())
             response = sock.recv(1024)
+            logging.info(f'client receives: {response}')
             return response
 
     def test_powerfullity(self):
         for i in range(5):
             response = self._send(Envelope(dict(command="powerfullity")))
             envelope = Envelope.from_bytes(response)
-            self.assertEqual(envelope.meta["powerfullity"], POWERFULLITY)
+            self.assertEqual(get_meta_attr(envelope.meta, "powerfullity"), POWERFULLITY)
 
     def tearDown(self) -> None:
-        self._send(Envelope(dict(command="stop")))
+        # self._send(Envelope(dict(command="stop")))
+        self.server.shutdown()
+        self.server.server_close()
         self.process.join()
