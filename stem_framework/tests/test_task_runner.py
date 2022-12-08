ba61d71435feefbc3dadf4de1de7aa082c60194c
Batmaev <vbbatmaev@gmail.com>
2022-12-08 13:28:38 +0300

    task 9 test

--- stem_framework/tests/test_task_runner.py
+++ stem_framework/tests/test_task_runner.py
@@ -1,7 +1,10 @@
 from unittest import TestCase
 
+from stem.task import FunctionDataTask, data, task
 from stem.task_master import TaskMaster
 from stem.task_runner import SimpleRunner, TaskRunner, ThreadingRunner, AsyncRunner, ProcessingRunner
+from stem.workspace import Workspace
+
 from tests.example_task import int_scale
 
 
@@ -25,6 +28,37 @@ class RunnerTest(TestCase):
         runner = AsyncRunner()
         self._run(runner)
 
+
     def test_process(self):
+        # Поскольку multiprocessing использует pickle,
+        # а pickle не может работать с декораторами, лямбда-функциями, замыканиями и тд
+        # будем тестировать этот раннер на специальном безопасном воркспейсе
+
         runner = ProcessingRunner()
-        self._run(runner)
+        master = TaskMaster(runner)
+        result = master.execute({}, int_scale_task)
+        print(result.status)
+
+        for i, r in zip(range(0, 100, 10), result.data):
+            self.assertEqual(i, r)
+
+
+
+# Picklable Workspace
+
+def int_scale_func(meta, int_range, data_scale):
+    return [data_scale * x for x in int_range]
+
+int_scale_task = task(int_scale_func)
+
+
+def int_range_func(meta):
+    return list(range(10))
+
+int_range = FunctionDataTask('int_range', int_range_func)
+
+
+def data_scale_func(meta):
+    return 10
+
+data_scale = FunctionDataTask('data_scale', data_scale_func)
